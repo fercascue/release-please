2ca02720db5e97abc6bf5aab4f3dea614724573f
fercascue <fercascue@gmail.com>
2023-10-12 10:16:12 +0200

    BREAKING CHANGE: new major version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload later or in the future.
-          Adding a breaking change
+          Adding a breaking change 2
         </p>
         <a
           className="App-link"
