8e247c5209c67eefd5b3c05f4222c13d9839a795
fercascue <fercascue@gmail.com>
2023-10-21 08:57:31 +0200

    feat: lint

--- sample-folder/src/App.js
+++ sample-folder/src/App.js
@@ -1,29 +1,26 @@
 import logo from './logo.svg'
 import './App.css'
 
-const a = 0
-a = 1
-
 function App () {
   return (
     <div className='App'>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload later or in the future.
           Adding a breaking change 2 3 4 5 6 7 8 9 10 11 12 14 15 16 17 18 19 20 21
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
