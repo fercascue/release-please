02d572ad34dc9a2d74f40a3f713af1a2e340222f
fercascue <fercascue@gmail.com>
2023-10-21 08:56:08 +0200

    feat: lint

--- sample-folder/src/App.js
+++ sample-folder/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-const a = 0;
-a = 1;
+const a = 0
+a = 1
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
